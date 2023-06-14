7792d89bd5d9b66556c176e46449998a65477d96
Andrew Smith <meuzishun@gmail.com>
2023-06-14 19:03:12 -0400

    Create post preview component

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,14 +1,10 @@
 const apiRoot = import.meta.env.VITE_API_ROOT;
 import styles from './Posts.module.css';
 import { useFetch } from '../../hooks/useFetch';
-import { Link } from 'react-router-dom';
+import PostPreview from './subcomponents/PostPreview';
 
 function Posts() {
-  const [data, error] = useFetch(apiRoot + '/posts', {
-    headers: {
-      Authorization: localStorage.getItem('token'),
-    },
-  });
+  const [data, error] = useFetch(apiRoot + '/posts');
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -20,31 +16,9 @@ function Posts() {
 
   return (
     <div className={styles.posts}>
-      {data.posts.map((post) => {
-        const formattedDate = new Date(post.timestamp)
-          .toLocaleString()
-          .split(',');
-
-        if (!post) {
-          return null;
-        }
-
-        return (
-          <div key={post._id}>
-            <h2>{post.title}</h2>
-            <div className='details'>
-              <p>
-                by {post.author.firstName} {post.author.lastName}
-              </p>
-              <p>
-                {formattedDate[0]}, {formattedDate[1]}
-              </p>
-            </div>
-            <p>{post.content}</p>
-            <Link to={'/post/' + post._id}>Continue reading</Link>
-          </div>
-        );
-      })}
+      {data.posts.map((post) => (
+        <PostPreview key={post.id} post={post} />
+      ))}
     </div>
   );
 }
---
+++ src/components/Posts/subcomponents/PostPreview.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import styles from './PostPreview.module.css';
+import { Link } from 'react-router-dom';
+
+function PostPreview({ post }) {
+  const formattedDate = new Date(post.timestamp).toLocaleString().split(',');
+
+  if (!post) {
+    return null;
+  }
+
+  return (
+    <div className={styles.post}>
+      <h2>{post.title}</h2>
+      <div className='details'>
+        <p>
+          by {post.author.firstName} {post.author.lastName}
+        </p>
+        <p>
+          {formattedDate[0]}, {formattedDate[1]}
+        </p>
+      </div>
+      <p>{post.content}</p>
+      <Link to={'/post/' + post._id}>Continue reading</Link>
+    </div>
+  );
+}
+
+PostPreview.propTypes = {
+  post: PropTypes.object.isRequired,
+};
+
+export default PostPreview;
