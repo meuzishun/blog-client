6f25741345e29f4810e6684e97f58420ce47451c
Andrew Smith <meuzishun@gmail.com>
2023-06-15 08:31:30 -0400

    Create separate subcomponent for nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,15 @@
 import { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../App';
 import styles from './Header.module.css';
+import Nav from './subcomponents/Nav';
 
 function Header() {
-  const { user, setUser } = useContext(UserContext);
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    console.log('Logging out...');
-    localStorage.clear();
-    setUser(null);
-    navigate('/');
-  };
+  const { user } = useContext(UserContext);
 
   return (
     <header className={styles.header}>
       <h1>Welcome {user ? `${user.firstName}...` : 'to the Blog'}</h1>
-      <nav>
-        <Link to='/'>Home</Link>
-        {!user ? (
-          <>
-            <Link to='register'>Register</Link>
-            <Link to='login'>Login</Link>
-          </>
-        ) : (
-          <Link onClick={handleLogout}>Logout</Link>
-        )}
-      </nav>
+      <Nav />
     </header>
   );
 }
---
+++ src/components/Header/subcomponents/Nav.jsx
@@ -0,0 +1,32 @@
+import { useContext } from 'react';
+import { UserContext } from '../../../App';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './Nav.module.css';
+
+function Nav() {
+  const { user, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    console.log('Logging out...');
+    localStorage.clear();
+    setUser(null);
+    navigate('/');
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <Link to='/'>Home</Link>
+      {!user ? (
+        <>
+          <Link to='register'>Register</Link>
+          <Link to='login'>Login</Link>
+        </>
+      ) : (
+        <Link onClick={handleLogout}>Logout</Link>
+      )}
+    </nav>
+  );
+}
+
+export default Nav;
