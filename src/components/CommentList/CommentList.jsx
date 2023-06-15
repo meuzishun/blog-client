a5db697a98f393daece6ad0d8e970bd0f4b82ccb
Andrew Smith <meuzishun@gmail.com>
2023-06-15 09:56:21 -0400

    Separate comment list and comment components

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,5 +1,39 @@
+const apiRoot = import.meta.env.VITE_API_ROOT;
+import styles from './CommentList.module.css';
+import { useFetch } from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import Comment from './subcomponents/Comment/Comment';
+
 function CommentList() {
-  return <div>CommentList</div>;
+  const params = useParams();
+  const [data, error] = useFetch(
+    apiRoot + '/posts/' + params.postId + '/comments/',
+    {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    }
+  );
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className={styles.commentList}>
+      {data.comments.length < 1 ? (
+        <p className={styles.firstCommentMessage}>Be the first to comment!</p>
+      ) : (
+        data.comments.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))
+      )}
+    </div>
+  );
 }
 
 export default CommentList;
