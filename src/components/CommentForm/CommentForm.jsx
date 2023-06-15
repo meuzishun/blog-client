e193c419eddda8da0f15b415692825565ff0520d
Andrew Smith <meuzishun@gmail.com>
2023-06-15 17:20:54 -0400

    Make comment form functional

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,11 +1,46 @@
+const apiRoot = import.meta.env.VITE_API_ROOT;
 import styles from './CommentForm.module.css';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
 
 function CommentForm({ setShowCommentForm }) {
+  const params = useParams();
+
+  const handleCommentSubmit = async (e) => {
+    e.preventDefault();
+
+    const comment = {
+      content: e.target.comment.value,
+    };
+
+    const response = await fetch(
+      apiRoot + '/posts/' + params.postId + '/comments',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('token'),
+        },
+        body: JSON.stringify(comment),
+      }
+    );
+
+    if (!response.ok) {
+      console.log(response);
+    }
+
+    window.location.reload();
+  };
+
   return (
-    <div className={styles.commentForm}>
-      <input type='text' placeholder='type a comment' />
-      <button>submit</button>
+    <form className={styles.commentForm} onSubmit={handleCommentSubmit}>
+      <input
+        type='text'
+        placeholder='type a comment'
+        id='comment'
+        name='comment'
+      />
+      <button type='submit'>submit</button>
       <button
         onClick={() => {
           setShowCommentForm(false);
@@ -13,7 +48,7 @@ function CommentForm({ setShowCommentForm }) {
       >
         cancel
       </button>
-    </div>
+    </form>
   );
 }
 
