c8e944648b275fde45f6a2d4f96756b275240cda
Andrew Smith <meuzishun@gmail.com>
2023-06-16 08:48:01 -0400

    Change name of blog in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <h1>Welcome to this Blog</h1>
+      <h1>You&apos;re Gonna Make Me Blog</h1>
       {!user ? null : <p>Hello {user.firstName}!</p>}
       <Nav />
     </header>
