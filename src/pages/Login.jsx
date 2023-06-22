a170dd5e80858c46656af69a1ca029ca4c722c45
Andrew Smith <meuzishun@gmail.com>
2023-06-22 19:30:01 -0400

    Fix link layout in forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-// import styles from './Form.module.css';
 import Form from '../components/Form/Form';
 
 function Login() {
@@ -7,7 +6,7 @@ function Login() {
     <div className='form-container'>
       <h2>Login</h2>
       <Form type='login' />
-      <div className='linkContainer'>
+      <div className='link-container'>
         <p>Don&apos;t have an account?</p>
         <Link to='/register'>Sign up</Link>
       </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-// import styles from './Form.module.css';
 import Form from '../components/Form/Form';
 
 function Register() {
@@ -7,7 +6,7 @@ function Register() {
     <div className='form-container'>
       <h2>Register</h2>
       <Form type='register' />
-      <div className='links-container'>
+      <div className='link-container'>
         <p>Already have an account?</p>
         <Link to='/login'>Login</Link>
       </div>
