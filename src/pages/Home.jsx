8f7e1b396d783de5cbc9d22ad55420105064852d
Andrew Smith <meuzishun@gmail.com>
2023-06-26 13:16:26 -0400

    Font colors clean up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,13 @@ function Home() {
   return (
     <div className='home'>
       <div className='hero'>
-        <header className='header'>
+        <div className='heading'>
           <p>Welcome to</p>
           <h1>You&apos;re Gonna Make Me Blog</h1>
           <p className='subheading'>
             Read rants about topics you pretend to care about
           </p>
-        </header>
+        </div>
         <nav>
           {!user ? (
             <>
