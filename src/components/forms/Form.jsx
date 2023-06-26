1671d88306d81d8d88429fd84f99b64c1955f3aa
Andrew Smith <meuzishun@gmail.com>
2023-06-26 13:26:58 -0400

    Return to home after login

--- src/components/forms/Form.jsx
+++ src/components/forms/Form.jsx
@@ -30,7 +30,7 @@ function Form({ type }) {
     localStorage.setItem('token', data.jwt.token);
     localStorage.setItem('user', userString);
     setUser(data.user);
-    navigate(-1);
+    navigate('/posts');
   };
 
   useEffect(() => {
