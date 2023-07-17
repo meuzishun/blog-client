2f8c14782eef1a29062cc1b526cfcd77311611d5
Andrew Smith <meuzishun@gmail.com>
2023-07-17 14:48:33 -0400

    Refactor CommentsContainer component to use useReducer hook

--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -1,38 +1,95 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import CommentList from './CommentList';
 import NewCommentForm from '../forms/NewCommentForm';
 import { getComments, submitComment } from '../../api/api';
 
+const initialCommentsState = {
+  isLoading: false,
+  isSubmitting: false,
+  loadError: null,
+  submitError: null,
+  comments: [],
+  showCommentForm: false,
+};
+
+function commentsReducer(state, action) {
+  switch (action.type) {
+    case 'loading':
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case 'loadError':
+      return {
+        ...state,
+        loadError: action.loadError,
+        isLoading: false,
+      };
+    case 'loadSuccess':
+      return {
+        ...state,
+        loadError: null,
+        isLoading: false,
+        comments: action.comments,
+      };
+    case 'draft':
+      return {
+        ...state,
+        showCommentForm: true,
+      };
+    case 'cancel':
+      return {
+        ...state,
+        showCommentForm: false,
+      };
+    case 'submitting':
+      return {
+        ...state,
+        isSubmitting: true,
+        submitError: null,
+      };
+    case 'submitError':
+      return {
+        ...state,
+        isSubmitting: false,
+        submitError: action.submitError,
+      };
+    case 'submitSuccess':
+      return {
+        ...state,
+        isSubmitting: false,
+        submitError: null,
+        showCommentForm: false,
+      };
+    default:
+      return initialCommentsState;
+  }
+}
+
 export default function CommentsContainer() {
   const params = useParams();
-  const [comments, setComments] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [showCommentForm, setShowCommentForm] = useState(false);
+  const [state, dispatch] = useReducer(commentsReducer, initialCommentsState);
 
   const loadComments = async () => {
-    setError(null);
-    setIsLoading(true);
+    dispatch({ type: 'loading' });
     const response = await getComments(params.postId);
 
     if (!response.ok) {
-      setError(response.statusText);
-      setIsLoading(false);
+      dispatch({ type: 'loadError', loadError: response.statusText });
     } else {
       const data = await response.json();
-      setComments(data.comments);
-      setError(null);
-      setIsLoading(false);
+      dispatch({ type: 'loadSuccess', comments: data.comments });
     }
   };
 
   const handleAddCommentClick = () => {
-    setShowCommentForm(true);
+    dispatch({ type: 'draft' });
   };
 
   const handleCommentSubmit = async (e) => {
     e.preventDefault();
+    dispatch({ type: 'submitting' });
     const response = await submitComment(
       {
         content: e.target.comment.value,
@@ -41,15 +98,15 @@ export default function CommentsContainer() {
     );
 
     if (!response.ok) {
-      console.log(response);
+      dispatch({ type: 'submitError', submitError: response.statusText });
     } else {
+      dispatch({ type: 'submitSuccess' });
       loadComments();
-      setShowCommentForm(false);
     }
   };
 
   const handleCommentCancel = () => {
-    setShowCommentForm(false);
+    dispatch({ type: 'cancel' });
   };
 
   useEffect(() => {
@@ -58,7 +115,7 @@ export default function CommentsContainer() {
 
   return (
     <>
-      {!showCommentForm ? (
+      {!state.showCommentForm ? (
         <button onClick={handleAddCommentClick} className='add-comment-btn'>
           Add a Comment
         </button>
@@ -66,9 +123,15 @@ export default function CommentsContainer() {
         <NewCommentForm
           handleCommentSubmit={handleCommentSubmit}
           handleCommentCancel={handleCommentCancel}
+          isSubmitting={state.isSubmitting}
+          submitError={state.submitError}
         />
       )}
-      <CommentList error={error} isLoading={isLoading} comments={comments} />
+      <CommentList
+        isLoading={state.isLoading}
+        loadError={state.loadError}
+        comments={state.comments}
+      />
     </>
   );
 }
