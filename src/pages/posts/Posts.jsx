7a72563347a0992afdd6aaadc9f89ef97ec91218
Andrew Smith <meuzishun@gmail.com>
2023-07-17 09:56:42 -0400

    Refactor Posts page to use useReducer hook

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -1,27 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { getPosts } from '../../api/api';
 import { Link } from 'react-router-dom';
 import Loading from '../Loading';
 import Error from '../Error';
 
+const initialPostsState = {
+  isLoading: false,
+  error: null,
+  posts: [],
+};
+
+function postsReducer(state, action) {
+  switch (action.type) {
+    case 'initialize':
+      return {
+        error: null,
+        isLoading: true,
+        posts: [],
+      };
+    case 'error':
+      return {
+        error: action.error,
+        isLoading: false,
+        posts: [],
+      };
+    case 'success':
+      return {
+        error: null,
+        isLoading: false,
+        posts: action.posts,
+      };
+    default:
+      return initialPostsState;
+  }
+}
+
 export default function Posts() {
-  const [posts, setPosts] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [state, dispatch] = useReducer(postsReducer, initialPostsState);
 
   const loadPosts = async () => {
-    setError(null);
-    setIsLoading(true);
+    dispatch({ type: 'initialize' });
     const response = await getPosts();
 
     if (!response.ok) {
-      setError(response.statusText);
-      setIsLoading(false);
+      dispatch({ type: 'error', error: response.statusText });
     } else {
       const data = await response.json();
-      setPosts(data.posts);
-      setError(null);
-      setIsLoading(false);
+      dispatch({ type: 'success', posts: data.posts });
     }
   };
 
@@ -29,17 +54,17 @@ export default function Posts() {
     loadPosts();
   }, []);
 
-  if (error) {
-    return <Error error={error} />;
+  if (state.error) {
+    return <Error error={state.error} />;
   }
 
-  if (isLoading) {
+  if (state.isLoading) {
     return <Loading />;
   }
 
   return (
     <div className='posts'>
-      {posts.map((post) => {
+      {state.posts.map((post) => {
         const formattedDate = new Date(post.timestamp)
           .toLocaleString()
           .split(',');
